6f852e8f8907cfdedc42a7f46be8b574ef29b9e9
João Eduardo Levandoski <joao.levandoski@lnls.br>
2019-11-04 17:29:24 -0300

    Metodo da bisseção implementado

---
+++ bissec_meth.py
@@ -0,0 +1,32 @@
+
+def function(x):
+    f = float(x**3 - x - 2)
+    return f
+
+def zero(a, b, err=0.01, max_inter=100):
+    n = 0
+    
+    if function(a) * function(b) > 0:
+        print('Intervalo errado')
+        return None
+
+    while n < max_inter:
+        med = (a+b)/2
+
+        if function(med) == 0 or abs((a-b)/2) < err:
+            return med
+        elif function(a) * function(med) < 0:
+            b = med
+        else:
+            a = med
+
+        n += 1
+
+    print(f'Excedeu o número de interações máximo. ({max_inter} {n})')
+    return None
+ 
+
+
+if __name__ == "__main__":
+
+    print(zero(a=1, b=2, err=0.0001))
